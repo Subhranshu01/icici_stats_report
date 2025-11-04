d6c11e5553629acea2232233942191190b6f388e
subhranshu moharana <86176478+Subhranshu01@users.noreply.github.com>
2025-11-04 12:38:47 +0530

    Refactor metric handling and update missing values
    Updated the handling of missing values in records by replacing 0 with 'NA'. Removed the calculation for the 99th percentile.

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import os
+fimport os
 import requests
 import pandas as pd
 from datetime import datetime, timedelta
@@ -94,8 +94,6 @@ def fetch_and_store_metrics(controller_name, url, api_token):
                 data_dict[method_name]["p90"] = round(value / 1_000_000, 2)
             elif "percentile(95.0)" in metric_id:
                 data_dict[method_name]["p95"] = round(value / 1_000_000, 2)
-            elif "percentile(99.0)" in metric_id:
-                data_dict[method_name]["p99"] = round(value / 1_000_000, 2)
             elif "errors.server.rate" in metric_id:
                 data_dict[method_name]["failure_rate"] = round(value, 2)
 
@@ -104,13 +102,12 @@ def fetch_and_store_metrics(controller_name, url, api_token):
         records.append({
             "Date": yesterday_str,
             "request": method,
-            "total hits": values.get("total_hits", 0),
-            "failure count": values.get("failure_count", 0),
-            "average": values.get("avg", 0),
-            "p90": values.get("p90", 0),
-            "p95": values.get("p95", 0),
-            "p99": values.get("p99", 0),
-            "failure rate": values.get("failure_rate", 0)
+            "total hits": values.get("total_hits", "NA"),
+            "failure count": values.get("failure_count", "NA"),
+            "average": values.get("avg", "NA"),
+            "p90": values.get("p90", "NA"),
+            "p95": values.get("p95", "NA"),
+            "failure rate": values.get("failure_rate", "NA")
         })
 
     df = pd.DataFrame(records)
