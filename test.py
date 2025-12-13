0617ade90ce7ba38d7b40d5383e5821fa2f5aabf
subhranshu moharana <86176478+Subhranshu01@users.noreply.github.com>
2025-12-13 23:40:43 +0530

    Add HSC URL fetching and metrics storing

--- test.py
+++ test.py
@@ -195,6 +195,7 @@ if __name__ == "__main__":
     Vehicle_url = os.environ["VEHICLE_URL"]
     SimpliInvest_url = os.environ["SIMPLY_URL"]
     AI_url = os.environ["AI_URL"]
+    HSC_url = os.environ["HSC_URL"]
 
     
 
@@ -221,6 +222,7 @@ if __name__ == "__main__":
     fetch_and_store_metrics("Vehicle Track", Vehicle_url, API_TOKEN)
     fetch_and_store_metrics("Simpli Invest", SimpliInvest_url, API_TOKEN)
     fetch_and_store_metrics("AI Finance", AI_url, API_TOKEN)
+    fetch_and_store_metrics("HSC", HSC_url, API_TOKEN)
 
     apply_formatting(FILE_NAME)
     send_email_report()
